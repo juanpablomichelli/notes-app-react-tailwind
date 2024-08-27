7ebe0ae3e8c6d58a03d0c80944944d789f8173e3
juanpablomichelli <juampimendoza@gmail.com>
2024-08-27 14:16:52 -0300

    Render context menu to the left when note is on the right side of the page to avoid overflowing

--- src/components/NoteContextMenu.jsx
+++ src/components/NoteContextMenu.jsx
@@ -8,7 +8,32 @@ export default function NoteContextMenu({
   menuRef,
   createImgUrl,
 }) {
-  const [contextMenuWidth, setContextMenuWidth] = useState(0);
+  const [contextMenuStyle, setContextMenuStyle] = useState({
+    right: 0,
+    left: 0,
+  });
+
+  //Determine menu position depending on which side of page the note is
+  useLayoutEffect(() => {
+    //get vw
+    let vw = window.innerWidth;
+    //get x position of note or x position of menu's left client rect
+    let menuLeft = menuRef.current.getBoundingClientRect().left;
+
+    setContextMenuStyle(() => {
+      if (menuLeft > vw / 2) {
+        return {
+          right: "initial",
+          left: "-20px",
+        };
+      } else {
+        return {
+          right: `-${menuRef.current.offsetWidth}px`,
+          left: "initial",
+        };
+      }
+    });
+  }, [menuRef]);
 
   useEffect(() => {
     let fileRef = fileInputRef.current;
@@ -22,21 +47,12 @@ export default function NoteContextMenu({
     };
   }, [createImgUrl, fileInputRef]);
 
-  useLayoutEffect(() => {
-    if (isOpen && menuRef.current) {
-      setContextMenuWidth(() => {
-        return menuRef.current.offsetWidth;
-      });
-    }
-  }, []);
-
-  let menuStyle = { right: `-${contextMenuWidth}px` };
   const liClass = "hover:bg-orange-200 pl-2";
   const menuClass = `absolute w-full min-w-[150px] bg-orange-100 border-2 border-black rounded-lg top-[-2px] z-10 text-sm md:text-base`;
   return (
     <>
       {isOpen && (
-        <menu ref={menuRef} className={menuClass} style={menuStyle}>
+        <menu ref={menuRef} className={menuClass} style={contextMenuStyle}>
           <li className={liClass}>
             <input
               ref={fileInputRef}
