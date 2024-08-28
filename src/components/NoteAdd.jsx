4f64ef9f331060bce54782700c6401bb87d916d5
juanpablomichelli <juampimendoza@gmail.com>
2024-08-28 14:23:39 -0300

    Minor style changes

--- src/components/NoteAdd.jsx
+++ src/components/NoteAdd.jsx
@@ -6,8 +6,11 @@ import { NotesContext } from "../context/NotesContext";
 export default function NoteAdd() {
   const { handleAddNote } = useContext(NotesContext);
   return (
-    <section className="w-full flex justify-center items-center">
-      <button onClick={handleAddNote} className="w-24 h-8 bg-orange-500 rounded-md">
+    <section className="w-full flex justify-center items-center bg-orange-100 fixed bottom-0 left-0 p-4">
+      <button
+        onClick={handleAddNote}
+        className="w-24 h-8 bg-orange-300 rounded-md border-black border-2"
+      >
         <FontAwesomeIcon icon={faPlus} />
       </button>
     </section>
